b3b9983100ba01ca6e2949a7de73ce7c290e4617
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-10-31 20:49:17 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new()
 
     def __str__(self):
         """Devuelve una representación de cadena del objeto."""
