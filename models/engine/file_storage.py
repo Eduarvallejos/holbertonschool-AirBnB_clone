734441b281021509cda98eb31177afbf6daaa7e5
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-10-31 21:56:06 -0500

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,7 +34,8 @@ class FileStorage:
         """
         Serializa __objects en el archivo JSON.
         """
-        objetos_serializador = {}
+        objetos_serializados = {}
+        
         for key, obj in FileStorage.__objects.items():
             objetos_serializados[key] = obj.to_dict()
 
