31b1755fdccadbc23e38fe2f163b92fc9a81d619
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-01 21:35:13 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -37,7 +37,7 @@ class BaseModel:
                 self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
