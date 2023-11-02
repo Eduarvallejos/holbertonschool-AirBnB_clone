49d2f632139aa476adb5bd08c35ef4bfc05927e2
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-01 21:40:08 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,6 @@ class BaseModel:
         Args:
             **Kwargs: Resibe un diccionario:
         """
-        self.id = str(uuid.uuid4())
         if kwargs:
             for k, v in kwargs.items():
                 if k != "__class__":
@@ -38,7 +37,7 @@ class BaseModel:
                 self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
 
         else:
-            
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
