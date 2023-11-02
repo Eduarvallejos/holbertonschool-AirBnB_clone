344aef31063b6b20da0a91fa23dc1209ceeda051
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-01 23:47:45 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,17 +26,20 @@ class BaseModel:
         Args:
             **Kwargs: Resibe un diccionario:
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if len(kwargs) != 0:
+        if kwargs:
             for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+                if k != "__class__":
+                    setattr(self, k, v)
+            
+            self.__dict__["created_at"] = datetime.strptime(
+                self.__dict__["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.__dict__["updated_at"] = datetime.strptime(
+                self.__dict__["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
