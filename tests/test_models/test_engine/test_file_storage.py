56cccdf3c01cefc28b2bcda6ed85761574585ec9
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-02 22:45:29 -0500

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 import unittest
 from unittest.mock import mock_open, patch
-from models.engine.file_storage import FileStorage  
+from models.engine.file_storage import FileStorage
+
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
-        """Preparación para las pruebas: crea una instancia de FileStorage."""
+        """
+        Preparación para las pruebas: crea una instancia de FileStorage.
+        """
         self.file_storage = FileStorage()
 
     def test_all(self):
-        """Prueba si el método all() devuelve un diccionario vacío inicialmente."""
+        """
+        Prueba si el método all() devuelve un diccionario vacío inicialmente.
+        """
         objects = self.file_storage.all()
         self.assertEqual(objects, {})
 
     def test_new(self):
-        """Prueba si el método new() agrega un objeto al diccionario __objects."""
+        """
+        Prueba si el método new() agrega un objeto al diccionario __objects.
+        """
         class DummyObject:
             def __init__(self, id):
                 self.id = id
@@ -26,7 +33,9 @@ class TestFileStorage(unittest.TestCase):
 
     @patch('builtins.open', new_callable=mock_open)
     def test_save(self, mock_file_open):
-        """Prueba si el método save() serializa objetos en un archivo JSON."""
+        """
+        Prueba si el método save() serializa objetos en un archivo JSON.
+        """
         class DummyObject:
         class DummyObject:
             def __init__(self, id):
@@ -38,14 +47,18 @@ class TestFileStorage(unittest.TestCase):
         self.file_storage.new(dummy_obj)
         self.file_storage.save()
 
-        """Verifica que la función 'open' se llamó con los argumentos correctos."""
+        """
+        Verifica que la función 'open' se llamó con los argumentos correctos.
+        """
         mock_file_open.assert_called_with('file.json', 'w')
         handle = mock_file_open()
         handle.write.assert_called_with('{"DummyObject.123": {"id": "123"}}')
 
     @patch('builtins.open', new_callable=mock_open, read_data='{"DummyObject.123": {"id": "123"}}')
     def test_reload(self, mock_file_open):
-        """Prueba si el método reload() deserializa un archivo JSON en objetos."""
+        """
+        Prueba si el método reload() deserializa un archivo JSON en objetos.
+        """
         self.file_storage.reload()
         objects = self.file_storage.all()
         self.assertIn('DummyObject.123', objects)
