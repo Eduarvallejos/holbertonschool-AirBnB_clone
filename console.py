d36d2ffc496083d6b682726325e931ff41c49051
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-03 20:43:05 -0500

    Update console.py

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Esta clase hereda de cmd.cmd."""
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Salir de la consola HBNB."""
