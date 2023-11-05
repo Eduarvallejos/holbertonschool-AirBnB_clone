918372c12dd2e324f97c81b1713fd9ab6c91903a
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-05 16:44:24 -0500

    Update console.py

--- console.py
+++ console.py
@@ -6,9 +6,45 @@ from models import storage
 from models.base_model import BaseModel
 
 
+def parse(arg):
+    dot1 = re.search(r"\{(.*?)\}", arg)
+    dot2 = re.search(r"\[(.*?)\]", arg)
+    if dot1 is None:
+        if dot2 is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            hbn = split(arg[:dot2.span()[0]])
+            retl = [i.strip(",") for i in hbn]
+            retl.append(dot2.group())
+            return retl
+    else:
+        hbn = split(arg[:dot1.span()[0]])
+        retl = [i.strip(",") for i in hbn]
+        retl.append(dot1.group())
+        return retl
+
 class HBNBCommand(cmd.Cmd):
     """Interprete de comandos"""
     prompt = "(hbnb) "
+
+    def default(self, arg):
+        """Sintaxis por defecto del modulo cmd si la entrada no es valida"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
 
     def do_quit(self, arg):
         """Salir del programa"""
@@ -27,31 +63,25 @@ class HBNBCommand(cmd.Cmd):
         Crea una nueva instancia de BaseModel;
         la guarda (en el archivo JSON) e imprime el ID.
         """
-        if not arg:
+        argl = parse(arg)
+        if len(argl) == 0:
             print("** class name missing **")
-        elif arg not in storage.classes:
-            print("** class doesn't exist **")
         else:
-            new_instance = storage.classes[arg]()
-            new_instance.save()
-            print(new_instance.id)
+            print(eval(argl[0])().id)
+            storage.save()
 
     def do_show(self, arg):
         """Print the string representation of an instance"""
-        args = shlex.split()
-        if not args:
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
             print("** class name missing **")
-        elif args[0] not in storage.classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
+        elif len(argl) == 1:
             print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict:
+            print("** no instance found **")
         else:
-            obj_id = args[1]
-            obj = storage.get(args[0], obj_id)
-            if obj in None:
-                print("** no instance found **")
-            else:
-                print(obj)
+            print(objdict["{}.{}".format(argl[0], argl[1])])
 
     def do_destroy(self, arg):
         """
@@ -77,54 +107,59 @@ class HBNBCommand(cmd.Cmd):
         """
         Imprimir todas las instancias o instancias de una clase específica.
         """
-        args = shlex.split(arg)
-        if not arg or not args:
-            objetos = storage.all()
-        elif args[0] not in storage.classes:
-            print("** la clase no existe **")
-            return
-        else:
-            objetos = storage.all(args[0])
-        print([str(obj) for obj in objetos.values()])
+        argl = parse(arg)
+
+        objl = []
+        for obj in storage.all().values():
+            if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                objl.append(obj.__str__())
+            elif len(argl) == 0:
+                objl.append(obj.__str__())
+        print(objl)
 
     def do_update(self, arg):
         """
         Actualizar una instancia basada en el nombre de la clase y el id.
         """
-        args = shlex.split(arg)
-        if not args:
-            print("** class name missing **")
-        elif args[0] not in storage.classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        elif len(args) < 3:
-            print("** attribute name missing **")
-        elif len(args) < 4:
-            print("** value missing **")
-        else:
-            class_name = args[0]
-            obj_id = args[1]
-            attribute_name = args[2]
-            attribute_value = args[3]
+        argl = parse(arg)
+        objdict = storage.all()
 
-            obj = storage.get(class_name, obj_id)
-            if obj is None:
-                print("** no instance found **")
+        if len(argl) == 0:
+            print("** class name missing **")
+            return False
+        if len(argl) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+            return False
+        if len(argl) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(argl) == 3:
+            try:
+                type(eval(argl[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        if len(argl) == 4:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            if argl[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[argl[2]])
+                obj.__dict__[argl[2]] = valtype(argl[3])
             else:
-                """
-                Verificar que el atributo no sea 'id', 'created_at' o
-                'updated_at'
-                """
-                if attribute_name not in ['id', 'created_at', 'updated_at']:
-                    """
-                    Convertir el valor del atributo al tipo apropiado.
-                    """
-                    if hasattr(obj, attribute_name):
-                        setattr(obj, attribute_name, attribute_value)
-                        obj.save()
+                obj.__dict__[argl[2]] = argl[3]
+        elif type(eval(argl[2])) == dict:
+            obj = objdict["{}.{}".format(argl[0], argl[1])]
+            for k, v in eval(argl[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
                 else:
-                    print("** el atributo no puede ser actualizado **")
+                    obj.__dict__[k] = v
+        storage.save()
 
 
 if __name__ == '__main__':
