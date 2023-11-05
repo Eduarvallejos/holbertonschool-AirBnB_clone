77dd55456d45b12d8e9e0998f0ad1e56e466322c
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-04 23:48:28 -0500

    Update console.py

--- console.py
+++ console.py
@@ -19,6 +19,17 @@ class HBNBCommand(cmd.Cmd):
         """No haga nada en una línea vacía"""
         pass
 
+    def do_create(self, arg):
+        """Crea una nueva instancia de BaseModel, la guarda (en el archivo JSON) e imprime el ID."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = HBNBCommand.__classes[arg]()
+            new_instance.save()
+            print(new_instance.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
