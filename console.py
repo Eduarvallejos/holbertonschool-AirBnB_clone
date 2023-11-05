4905ac1b676e2fdedee097170a64a2eb9fe7a9ce
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-05 17:16:44 -0500

    Update console.py

--- console.py
+++ console.py
@@ -109,7 +109,7 @@ class HBNBCommand(cmd.Cmd):
         Imprimir todas las instancias o instancias de una clase específica.
         """
         argl = parse(arg)
-        if len(argl) > 0 and argl[0] not in models.storage:
+        if len(argl) > 0 and argl[0] not in BaseModel:
             print("** class doesn't exist **")
         else:
             objl = []
