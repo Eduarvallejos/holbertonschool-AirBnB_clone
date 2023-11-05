f1a7ef342493e13a7bd7e90df97500e67144b733
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-11-05 16:52:40 -0500

    Update console.py

--- console.py
+++ console.py
@@ -109,14 +109,16 @@ class HBNBCommand(cmd.Cmd):
         Imprimir todas las instancias o instancias de una clase específica.
         """
         argl = parse(arg)
-
-        objl = []
-        for obj in storage.all().values():
-            if len(argl) > 0 and argl[0] == obj.__class__.__name__:
-                objl.append(obj.__str__())
-            elif len(argl) == 0:
-                objl.append(obj.__str__())
-        print(objl)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
 
     def do_update(self, arg):
         """
