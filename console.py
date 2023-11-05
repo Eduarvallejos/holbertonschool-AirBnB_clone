c829eef2b7662c3c83a93c6f4775e5534f383ae7
root <manuelvallejos2018@gmail.com>
2023-11-04 18:31:56 -0700

    updated console

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""Definimos la clase HBNBCommand."""
+import cmd
 
+
+class HBNBCommand(cmd.Cmd):
+    """Interprete de comandos"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Salir del programa"""
+        return True
+
+    def do_EOF(self, arg):
+        """Salir del programa"""
+        return True
+    
+    def emptyline(self):
+        """No haga nada en una línea vacía"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
