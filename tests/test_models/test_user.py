833e046b497cc64f334bb53a391d85a634496e4a
root <manuelvallejos2018@gmail.com>
2023-11-05 17:42:25 -0800

    creando test para user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+
+    def test_attributes(self):
+        """
+        Verificar que los atributos de User se inicializan correctamente.
+        """
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+
+    def test_inheritance(self):
+        """Verificar que User hereda de BaseModel."""
+        user = User()
+        self.assertTrue(issubclass(User, BaseModel))
+
+
+if __name__ == "__main__":
+    unittest.main()
